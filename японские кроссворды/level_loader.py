e6ba78a216a43353b472ee3e654032e47fc45b27
juunaarrr <juunaarrr@gmail.com>
2026-04-12 21:29:44 +0300

    Update level_loader.py

--- японские кроссворды/level_loader.py
+++ японские кроссворды/level_loader.py
@@ -3,11 +3,12 @@ import os
 
 
 def load_level(level_number):
-    filename = f"levels/level1.json"
+    filename = f"levels/level{level_number}.json"  # ← используем level_number
 
     if not os.path.exists(filename):
         print(f"Файл {filename} не найден")
         return None
 
     with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+        data = json.load(f)
+        return data
